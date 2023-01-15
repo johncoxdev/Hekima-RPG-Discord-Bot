cc9bb175f0ab7119eaca8fa8192d823fa368ae16
programwithjohn <john.cox0321@gmail.com>
2023-01-14 19:19:27 -0600

    switched the guild based deploying to global

--- deploy-commands.js
+++ deploy-commands.js
@@ -18,7 +18,7 @@ for (const subFolder of commandFolders){
 }
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
 
